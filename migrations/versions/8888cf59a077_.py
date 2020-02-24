b78abf437536b177ebda3f421eb865de043cb4fe
Eduard Chiticari <eduard.kitikari@gmail.com>
2020-02-24 18:31:34 +0000

    comma added

--- migrations/versions/8888cf59a077_.py
+++ migrations/versions/8888cf59a077_.py
@@ -30,7 +30,7 @@ def upgrade():
     sa.Column('public_id', sa.String(length=50), nullable=True),
     sa.Column('name', sa.String(length=50), nullable=True),
     sa.Column('password', sa.String(length=80), nullable=True),
-    sa.Column('email', sa.String(length=50))
+    sa.Column('email', sa.String(length=50)),
     sa.Column('admin', sa.Boolean(), nullable=True),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('public_id')
